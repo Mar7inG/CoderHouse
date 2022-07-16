6f9aa7a4b5712120c7411b19fc20bad6ac95649e
Martin Goñi <mar7in.goni@gmail.com>
2022-07-16 01:53:09 -0300

    No me abre la key del diccionario

--- Proyecto1/Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/Proyecto1/views.py
@@ -3,10 +3,10 @@ from django.template import Context, Template
 
 def template1(request):
 
-    mihtml=open("C:/Users/Tin/Documents/ArVSC/CoderHouse/Proyecto1/Proyecto1/Plantillas/Template.html")
+    mihtml=open("C:/Users/Tin/Documents/ArVSC/CoderHouse/CoderHouse/Proyecto1/Proyecto1/Proyecto1/Plantillas/Template.html")
     plantilla=Template(mihtml.read())
     mihtml.close
-    miContexto=Context()
+    miContexto=Context({"Mi nombre":"Martin"})
     documento=plantilla.render(miContexto)
     return HttpResponse(documento)
     
